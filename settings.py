7b939e93a5bc02bbd86f09d81f1e1b42ebd1a0ac
Propoy Yaroslav <yaroslavpropoy@gmail.com>
2016-03-23 23:17:45 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,6 +2,6 @@ DOMAIN = {'people': {}}
 
 MONGO_HOST = 'localhost'
 MONGO_PORT = 27017
-MONGO_USERNAME = 'admin'
-MONGO_PASSWORD = 'admin'
+MONGO_USERNAME = 'user'
+MONGO_PASSWORD = 'user'
 MONGO_DBNAME = 'apitest'
